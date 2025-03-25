27ed8a174c31558ee8c239f1312da04a1f3fcb9a
h22krsni <121024659+krisni0209@users.noreply.github.com>
2025-03-25 20:46:18 +0100

    appbar
    appbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Link, Outlet } from 'react-router-dom';  // <-- Import Outlet here
+import {Box, AppBar, Toolbar, Typography, Button} from '@mui/material';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+        
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Link to="/">Hockey Shop</Link>
+          </Typography>
+          <Button color="inherit"><Link to="/Product/new">Nya Produkter</Link></Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+      <Outlet />  {/* This will render the matched child route */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import ProductEdit from './views/ProductEdit.jsx'
+import Products from './views/Products.jsx'
+import ProductDetail from './views/ProductDetail.jsx'
+import Home from './views/Home.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/Product/new', element: <ProductEdit /> },
+      { path: '/Products', element: <Products /> },
+      { path: '/ProductDetail', element: <ProductDetail /> }, 
+    ],
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
